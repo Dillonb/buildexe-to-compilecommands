1cf486b8937e545419130fdbf9a4187dc984c155
Dillon Beliveau <dillon@dgb.sh>
2026-02-09 00:04:33 -0800

    Add clippy/rustfmt to flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,29 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
       devShells = forAllSystems (system: {
         default = pkgs.${system}.mkShell {
-          buildInputs = with pkgs.${system}; [ cargo rustc rust-analyzer bacon ];
+          buildInputs = with pkgs.${system}; [
+            cargo
+            rustc
+            rust-analyzer
+            bacon
+            clippy
+            rustfmt
+          ];
         };
       });
     };
